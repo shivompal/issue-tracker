b0700546f477ef96a56e3970af509e284d29eecb
Shiv Om Pal <shivompal@gmail.com>
2023-10-09 16:40:59 +0530

    Updated NavBar component to style active link

---
+++ app/issues/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function IssuesPage() {
+    return (
+        <div>Issues Page</div>
+    )
+}
+
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,8 +1,14 @@
+"use client"
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 import React from "react"
 import { AiFillBug } from "react-icons/ai"
+import classnames from "classnames"
 
 export default function NavBar() {
+    const currentPath = usePathname()
+    console.log(currentPath)
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' }
@@ -16,7 +22,13 @@ export default function NavBar() {
                         <Link
                             key={link.href}
                             href={link.href}
-                            className="text-zinc-500 hover:text-zinc-800 transition-colors">{link.label}
+                            // className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'}  hover:text-zinc-800 transition-colors`}
+                            className={classnames({
+                                'text-zinc-900': link.href === currentPath,
+                                'text-zinc-500': link.href !== currentPath,
+                                'hover:text-zinc-800 transition-colors': true
+                            })}
+                        >{link.label}
                         </Link>)
                 }
             </ul>
