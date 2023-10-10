475dd4973572ea241c65ed640705b255ae893aab
Shiv Om Pal <shivompal@gmail.com>
2023-10-10 19:19:19 +0530

    Added loading skeleton

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,14 +1,11 @@
+import { Table } from '@radix-ui/themes'
 import React from 'react'
-import { Button, Table, TableColumnHeaderCell, TableRow } from '@radix-ui/themes'
-import Link from 'next/link'
-import prisma from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge'
-import delay from 'delay'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import IssuesActions from './IssuesActions'
 
-export default async function IssuesPage() {
-    const issues = await prisma.issue.findMany()
-    await delay(2000)
+export default function LoadingIssuesPageconst() {
+    const issues = [1, 2, 3, 4, 5]
     return (
         <div>
             <IssuesActions />
@@ -23,22 +20,20 @@ export default async function IssuesPage() {
                 <Table.Body>
                     {
                         issues.map(issue => (
-                            <Table.Row key={issue.id}>
+                            <Table.Row key={issue}>
                                 <Table.Cell>
-                                    {issue.title}
+                                    <Skeleton />
                                     <div className='block md:hidden'>
-                                        <IssueStatusBadge status={issue.status} />
+                                        <Skeleton />
                                     </div>
                                 </Table.Cell>
-                                <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
-                                <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+                                <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
+                                <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
                             </Table.Row>
                         ))
                     }
                 </Table.Body>
             </Table.Root>
         </div>
-
     )
 }
-
