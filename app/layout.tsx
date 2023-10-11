2bb75ce34643816adff9820cbbb4b9c517dc3929
Shiv Om Pal <shivompal@gmail.com>
2023-10-11 23:23:47 +0530

    Added login and logout links

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./navbar";
+import AuthProvider from "./auth/provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,13 +28,15 @@ export default function RootLayout({
       {/* <body className={inter.className}> */}
       <body className={inter.variable}>
         {/* <Theme appearance="light" accentColor="violet"> */}
-        <Theme accentColor="violet">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-          {/* <ThemePanel /> */}
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="violet">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+            {/* <ThemePanel /> */}
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -5,9 +5,12 @@ import { usePathname } from "next/navigation"
 import React from "react"
 import { AiFillBug } from "react-icons/ai"
 import classnames from "classnames"
+import { useSession } from "next-auth/react"
+import { Box } from "@radix-ui/themes"
 
 export default function NavBar() {
     const currentPath = usePathname()
+    const { status, data: session } = useSession()
     console.log(currentPath)
     const links = [
         { label: 'Dashboard', href: '/' },
@@ -19,19 +22,27 @@ export default function NavBar() {
             <ul className="flex space-x-6">
                 {
                     links.map(link =>
-                        <Link
-                            key={link.href}
-                            href={link.href}
-                            // className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'}  hover:text-zinc-800 transition-colors`}
-                            className={classnames({
-                                'text-zinc-900': link.href === currentPath,
-                                'text-zinc-500': link.href !== currentPath,
-                                'hover:text-zinc-800 transition-colors': true
-                            })}
-                        >{link.label}
-                        </Link>)
+                        <li key={link.href}>
+                            <Link
+                                href={link.href}
+                                // className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'}  hover:text-zinc-800 transition-colors`}
+                                className={classnames({
+                                    'text-zinc-900': link.href === currentPath,
+                                    'text-zinc-500': link.href !== currentPath,
+                                    'hover:text-zinc-800 transition-colors': true
+                                })}
+                            >{link.label}
+                            </Link>
+                        </li>)
                 }
             </ul>
+            <Box>
+                {status === 'authenticated' && (
+                    <Link href='/api/auth/signout'>Log out</Link>)}
+                {status === 'unauthenticated' && (
+                    <Link href='/api/auth/signin'>Log in</Link>)}
+
+            </Box>
         </nav>
     )
 }
