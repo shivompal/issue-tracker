267eb9783a2b61a8e84859243d6f4dfb6528de5c
Shiv Om Pal <shivompal@gmail.com>
2023-10-12 16:02:34 +0530

    Populated assignee DDL from DB

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,9 +1,22 @@
 "use client"
 
+import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export default function AssigneeSelect() {
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const { data } = await axios.get<User[]>('/api/users')
+            setUsers(data)
+        }
+
+        fetchUsers()
+    }, [])
+
     return (
         <Select.Root>
             <Select.Trigger placeholder='Assign' />
@@ -12,7 +25,9 @@ export default function AssigneeSelect() {
                     <Select.Label>
                         Suggestions
                     </Select.Label>
-                    <Select.Item value='Shiv Om Pal'>Shiv Om Pal</Select.Item>
+                    {users.map(user => (
+                        <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+                    ))}
                 </Select.Group>
             </Select.Content>
         </Select.Root>
