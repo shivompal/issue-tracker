c24e17368b9aed16d978945d91217debd1f43fa0
Shiv Om Pal <shivompal@gmail.com>
2023-10-13 21:02:40 +0530

    Extracted IssueTable component for issue page

--- app/issues/list/IssuesActions.tsx
+++ app/issues/list/IssuesActions.tsx
@@ -5,7 +5,7 @@ import IssueStatusFilter from "./IssueStatusFilter";
 
 export default function IssuesActions() {
   return (
-    <Flex mb="5" justify="between">
+    <Flex justify="between">
       <IssueStatusFilter />
       <Button>
         <Link href="/issues/new">New Issue</Link>
