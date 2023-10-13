6f9d34e66847af575eb89ee2d30b0b647ebf4bcd
Shiv Om Pal <shivompal@gmail.com>
2023-10-13 19:13:23 +0530

    Fixed filtering bugs

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -49,7 +49,10 @@ export default async function IssuesPage({ searchParams }: Props) {
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
                 <NextLink
                   href={{
                     query: { ...searchParams, orderBy: column.value },
