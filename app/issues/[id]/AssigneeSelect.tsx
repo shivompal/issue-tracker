f64fa87a81e985c923f541bf8037539d78bc5512
Shiv Om Pal <shivompal@gmail.com>
2023-10-13 13:51:53 +0530

    Refactored AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -5,20 +5,10 @@ import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 export default function AssigneeSelect({ issue }: { issue: Issue }) {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000,
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
@@ -33,20 +23,20 @@ export default function AssigneeSelect({ issue }: { issue: Issue }) {
 
   //     fetchUsers()
   // }, [])
-
+  const assignIssue = (userId: string) => {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId.trim() || null,
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved");
+      });
+  };
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || " "}
-        onValueChange={(userId) => {
-          axios
-            .patch("/api/issues/" + issue.id, {
-              assignedToUserId: userId.trim() || null,
-            })
-            .catch(() => {
-              toast.error("Changes could not be saved");
-            });
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign" />
         <Select.Content>
@@ -65,3 +55,11 @@ export default function AssigneeSelect({ issue }: { issue: Issue }) {
     </>
   );
 }
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
