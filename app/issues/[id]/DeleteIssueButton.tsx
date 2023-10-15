eb32663d3de52af5915e4b211c5218bf3f899f3a
Shiv Om Pal <shivompal@gmail.com>
2023-10-15 08:16:00 +0530

    Debug delete button issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -9,14 +9,27 @@ import { useState } from "react";
 export default function DeleteIssueButton({ issueId }: { issueId: number }) {
   const router = useRouter();
   const [error, setError] = useState(false);
-  const [deleting, setDeleting] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      setDeleting(true);
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues/list");
+      router.refresh();
+    } catch (error) {
+      setDeleting(false);
+      setError(true);
+    }
+  };
+
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red" disabled={deleting}>
+          <Button color="red" disabled={isDeleting}>
             Delete Issue
-            {deleting && <Spinner />}
+            {isDeleting && <Spinner />}
           </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
@@ -24,27 +37,14 @@ export default function DeleteIssueButton({ issueId }: { issueId: number }) {
           <AlertDialog.Description>
             Are you sure you want to delete this issue?
           </AlertDialog.Description>
-          <Flex className="mt-3" gap="3">
+          <Flex mt="4" gap="3" justify="end">
             <AlertDialog.Cancel>
               <Button variant="soft" color="gray">
                 Cancel
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button
-                color="red"
-                onClick={async () => {
-                  try {
-                    setDeleting(true);
-                    await axios.delete("/api/issues/" + issueId);
-                    router.push("/issues/list");
-                    router.refresh();
-                  } catch (error) {
-                    setDeleting(false);
-                    setError(true);
-                  }
-                }}
-              >
+              <Button color="red" onClick={deleteIssue}>
                 Delete Issue
               </Button>
             </AlertDialog.Action>
