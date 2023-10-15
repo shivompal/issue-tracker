800370433be48ff32368cb584d84a6cd33924102
Shiv Om Pal <shivompal@gmail.com>
2023-10-15 07:53:23 +0530

    Debug vercel issues

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,14 +1,13 @@
+import authOptions from "@/app/auth/authOptions";
 import prisma from "@/prisma/client";
 import { Box, Flex, Grid } from "@radix-ui/themes";
-import delay from "delay";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import { cache } from "react";
+import AssigneeSelect from "./AssigneeSelect";
+import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./DeleteIssueButton";
-import { getServerSession } from "next-auth";
-import authOptions from "@/app/auth/authOptions";
-import AssigneeSelect from "./AssigneeSelect";
-import { cache } from "react";
 
 interface Props {
   params: { id: string };
